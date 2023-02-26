d8bc29590da667ac60a2e1bec1db829a605153c7
LaBourgeoise <100392465+LaBourgeoise@users.noreply.github.com>
2023-02-26 18:36:57 +0000

    Removed println

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PlanGraphComponent } from './component/chart/plan-graph/plan-graph.component';
+
+
+const routes: Routes = [
+  { path: '', component: PlanGraphComponent }
+];
+
+@NgModule({
