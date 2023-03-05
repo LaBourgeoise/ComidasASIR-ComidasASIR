27870b36201df7fbf5c46f9acf888d1bc266ea2c
LaBourgeoise <100392465+LaBourgeoise@users.noreply.github.com>
2023-03-05 05:47:57 +0000

    Add readme and license.

--- src/app/component/chart/life-events/life-events.component.ts
+++ src/app/component/chart/life-events/life-events.component.ts
@@ -1,1 +1,30 @@
-import { Component, OnInit } from '@angular/
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-life-events',
+  templateUrl: './life-events.component.html',
+  styleUrls: ['./life-events.component.scss']
+})
+export class LifeEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  eventsList = [
+    {
+      'icon': 'icon-home',
+      'name': 'House',
+      'color': 'inset 50px 0px 0px 0px #cee9fe',
+      'goalTitle': 'New House',
+      'goalAmount': '12.25 Lac',
+      'goalYear': 'Jan 2030'
+    },
+    {
+      'icon': 'icon-book',
+      'name': 'Education',
+      'color': '"box-shadow":"inset 50px 0px 0px 0px #d0f2e7"',
+      'goalTitle': 'Masters',
+      'goalA
