f42b09d80dd4210fb889712aa1d4c0cd8b3dda56
LaBourgeoise <100392465+LaBourgeoise@users.noreply.github.com>
2023-03-15 06:37:47 +0000

    mingw: be more defensive when making the environment block

--- src/app/component/chart/plan-graph/plan-graph.component.ts
+++ src/app/component/chart/plan-graph/plan-graph.component.ts
@@ -1,2 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { EChartOption } f
+import { EChartOption } from 'echarts';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-plan-graph',
+  templateUrl: './plan-graph.component.html',
+  styleUrls: ['./plan-graph.component.scss']
+})
+export class PlanGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+/* There is no proper examples and explanation for the Chart. Most of the documentation related to this is in chinese*/
+/* Many of the sites related to this are blocked as it violates few of the GDPR rules */
+chartOption: EChartOption = {
+  tooltip : {
+      trigger: 'axis'
+  },
+  dataZoom : {
+      show : true,
+      realtime : true,
+      y: 560,
+      height: 20,
+      fillerCo
