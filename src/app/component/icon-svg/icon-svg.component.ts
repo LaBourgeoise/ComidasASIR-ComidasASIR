4d48bb5338f359ad78b2e1216509df2ce939c5cf
LaBourgeoise <100392465+LaBourgeoise@users.noreply.github.com>
2023-03-19 11:44:51 +0000

    scheduling

---
+++ src/app/component/icon-svg/icon-svg.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-svg',
+  styleUrls: ['./icon-svg.component.scss'],
+  template: `
+    <svg [ngStyle]="appliedStyle" class="iconStyle">
+      <use attr.xlink:href="assets/symbol-defs.svg#{{icon}}"></use>
+    </svg>`
+})
+export class IconSvgComponent implements OnInit {
+  @Input
